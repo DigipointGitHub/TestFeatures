3bad29803e658d08a164a5a9934413cc8ee5ecfc
Radaelli Marco <radaelli@digipoint.it>
2018-03-18 13:17:48 +0100

    Add project files.

---
+++ TestFeatures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
